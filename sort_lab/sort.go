2b936023e9aef1de9746649b372b4b39a98b965a
kanzihuang <kanzihuang@qq.com>
2023-05-19 13:33:04 +0800

    实现插入排序和快速排序

--- sort_lab/sort.go
+++ sort_lab/sort.go
@@ -1,1 +1,37 @@
 package sort_lab
+
+func insertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0 && arr[j-1] > arr[j]; j-- {
+			arr[j-1], arr[j] = arr[j], arr[j-1]
+		}
+	}
+}
+
+func quickSort(arr []int) {
+	if len(arr) <= 1 {
+		return
+	}
+
+	pivot := arr[0]
+	i, j := 1, len(arr)-1
+	for i <= j {
+		switch {
+		case arr[i] < pivot:
+			i++
+		case arr[j] >= pivot:
+			j--
+		default:
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	if j > 0 {
+		arr[0], arr[j] = arr[j], arr[0]
+	}
+	if j >= 2 {
+		quickSort(arr[:j])
+	}
+	if j <= len(arr)-3 {
+		quickSort(arr[j+1:])
+	}
+}
--- sort_lab/sort_test.go
+++ sort_lab/sort_test.go
@@ -1,20 +1,67 @@
 package sort_lab
 
-import "testing"
+import (
+	"math/rand"
+	"sort"
+	"testing"
+)
 
-func Test_insertSort(t *testing.T) {
-	type args struct {
-		arr []int
+func testSortBy(t *testing.T, data []int, sortName string, sortFunc func(data []int)) {
+	got := make([]int, len(data))
+	copy(got, data)
+	sortFunc(got)
+	if !sort.IntsAreSorted(got) {
+		t.Errorf("%s(%v) = %v, not sorted\n", sortName, data, got)
 	}
+}
+func TestSort(t *testing.T) {
+	cases := [][]int{
+		{1, 2, 3, 4, 5},
+		{5, 4, 3, 2, 1},
+		{3, 7, 2, 7, 11},
+		{74, 59, 238, -784, 9845, 959, 905, 0, 0, 42, 7586, -5467984, 7586},
+	}
+	for _, data := range cases {
+		testSortBy(t, data, "insertionSort", insertionSort)
+		testSortBy(t, data, "quickSort", quickSort)
+	}
+}
+
+func TestSortGreat(t *testing.T) {
+	num := 100000
+	data := generateRands(num)
+	sortedData := generateRands(num)
+	sort.Ints(sortedData)
+	reversedData := generateRands(num)
+	sort.Sort(sort.Reverse(sort.IntSlice(reversedData)))
+
 	tests := []struct {
 		name string
-		args args
+		sort func([]int)
 	}{
-		// TODO: Add test cases.
+		{name: "insertionSort", sort: insertionSort},
+		{name: "quickSort", sort: quickSort},
+		{name: "sort.Ints", sort: sort.Ints},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			insertSort(tt.args.arr)
+			testSortBy(t, data, tt.name, tt.sort)
+		})
+
+		t.Run(tt.name+"_sorted", func(t *testing.T) {
+			testSortBy(t, sortedData, tt.name, tt.sort)
+		})
+
+		t.Run(tt.name+"_reversed", func(t *testing.T) {
+			testSortBy(t, reversedData, tt.name, tt.sort)
 		})
 	}
+}
+
+func generateRands(num int) []int {
+	data := make([]int, num)
+	for i := range data {
+		data[i] = rand.Int()
+	}
+	return data
 }
