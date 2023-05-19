ab9fb5346ccd8ad628eaec3e4f682204522b229c
kanzihuang <kanzihuang@qq.com>
2023-05-19 13:41:30 +0800

    test struct

--- syntax_labs/struct_test.go
+++ syntax_labs/struct_test.go
@@ -13,3 +13,24 @@ func TestStructEmpty(t *testing.T) {
 		t.Errorf("want: a == b, but got %p, %p\n", a, b)
 	}
 }
+
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+func setNext(next **Node, node *Node) {
+	*next = node
+}
+
+func NewNode(value int) *Node {
+	return &Node{value: value}
+}
+
+func TestPoint(t *testing.T) {
+	root := NewNode(5)
+	root.left = NewNode(2)
+	root.right = NewNode(10)
+	setNext(&root.right, nil)
+}
