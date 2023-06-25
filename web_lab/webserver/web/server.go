6b9fcb4a719ded4aa38bb7353b499a26cbecf4f2
Wanli <kanzihuang@qq.com>
2023-06-25 08:31:59 +0800

    refactor web server 0.1

---
+++ web_lab/webserver/web/server.go
@@ -0,0 +1,25 @@
+package web
+
+import (
+	"net/http"
+)
+
+type sdkHttpServer struct {
+	Name    string
+	handler Handler
+}
+
+func (s *sdkHttpServer) Route(method, pattern string, handlerFunc func(c *Context)) {
+	s.handler.Route(method, pattern, handlerFunc)
+}
+
+func (s *sdkHttpServer) Start(address string) error {
+	return http.ListenAndServe(address, s.handler)
+}
+
+func NewServer() Server {
+	return &sdkHttpServer{
+		Name:    "sdkHttpServer",
+		handler: NewHandlerBasedOnMap(),
+	}
+}
