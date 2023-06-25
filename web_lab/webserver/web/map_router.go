d7009d06f99c045ecf65a116311bc1fa473db020
Wanli <kanzihuang@qq.com>
2023-06-25 09:42:37 +0800

    refactor web.Handler

---
+++ web_lab/webserver/web/map_router.go
@@ -0,0 +1,32 @@
+package web
+
+import "fmt"
+
+type RouterBasedOnMap struct {
+	handlers map[string]func(c *Context)
+}
+
+func NewRouterBaseOnMap() Router {
+	return &RouterBasedOnMap{
+		handlers: make(map[string]func(c *Context)),
+	}
+}
+
+func (r *RouterBasedOnMap) handle(method string, path string, context *Context) bool {
+	key := r.key(method, path)
+	if handler, ok := r.handlers[key]; ok {
+		handler(context)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (r *RouterBasedOnMap) key(method string, path string) string {
+	return fmt.Sprintf("%s#%s", method, path)
+}
+
+func (r *RouterBasedOnMap) Route(method string, pattern string, handlerFunc handlerFunc) {
+	key := r.key(method, pattern)
+	r.handlers[key] = handlerFunc
+}
