02fe297790883e1deb22f928cc09fc5cb95cf95c
Wanli <kanzihuang@qq.com>
2023-06-29 09:49:49 +0800

    implement filter in webserver

--- web_lab/webserver/web/server.go
+++ web_lab/webserver/web/server.go
@@ -5,21 +5,42 @@ import (
 )
 
 type sdkHttpServer struct {
-	Name    string
-	handler *Handler
+	Name   string
+	router Router
+	root   Filter
 }
 
 func (s *sdkHttpServer) Route(method, pattern string, handlerFunc HandlerFunc) {
-	s.handler.Route(method, pattern, handlerFunc)
+	s.router.Route(method, pattern, handlerFunc)
 }
 
 func (s *sdkHttpServer) Start(address string) error {
-	return http.ListenAndServe(address, s.handler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c := NewContext(w, r)
+		s.root(c)
+	})
+	return http.ListenAndServe(address, nil)
 }
 
-func NewServer() Server {
-	return &sdkHttpServer{
-		Name:    "sdkHttpServer",
-		handler: NewHandler(),
+func (s *sdkHttpServer) ServeHTTP(c *Context) {
+	if handlerFunc := s.router.FindHandlerFunc(c.R.Method, c.R.URL.Path); handlerFunc != nil {
+		handlerFunc(c)
+	} else {
+		c.W.WriteHeader(http.StatusNotFound)
+		_, _ = c.W.Write([]byte("Not any router matched"))
 	}
+}
+
+func NewServer(name string, builders ...FilterBuilder) Server {
+	server := &sdkHttpServer{
+		Name:   name,
+		router: NewRouterBasedOnTree(),
+	}
+	var root Filter = server.ServeHTTP
+	for i := len(builders) - 1; i >= 0; i-- {
+		b := builders[i]
+		root = b(root)
+	}
+	server.root = root
+	return server
 }
