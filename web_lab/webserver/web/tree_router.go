7d5e28b0c617dd2f7d7a5dac9512486d69161e9f
kanzihuang <kanzihuang@qq.com>
2023-07-01 22:23:44 +0800

    implement param route for webserver

--- web_lab/webserver/web/tree_router.go
+++ web_lab/webserver/web/tree_router.go
@@ -8,10 +8,10 @@ import (
 
 type Node interface {
 	findChild(name string) Node
-	findHandlerFunc(names []string) HandlerFunc
+	findHandlerFunc(path string, paths []string) HandlerFunc
 	isMatched(name string) bool
 	addChild(names []string, handlerFunc HandlerFunc)
-	getHandlerFunc() HandlerFunc
+	handlerFuncBuilder(name string, handlerFunc HandlerFunc) HandlerFunc
 	setHandlerFunc(handlerFunc HandlerFunc)
 }
 
@@ -28,14 +28,14 @@ type RouterBasedOnTree struct {
 }
 
 func (r *RouterBasedOnTree) Route(method string, pattern string, handlerFunc HandlerFunc) {
-	pattern = strings.Trim(pattern, "/")
+	pattern = strings.TrimRight(pattern, "/")
 	names := strings.Split(pattern, "/")
 	root, ok := r.forest[method]
 	if ok {
-		if len(names) == 1 && names[0] == "" {
+		if len(names) == 1 {
 			root.setHandlerFunc(handlerFunc)
 		} else {
-			root.addChild(names, handlerFunc)
+			root.addChild(names[1:], handlerFunc)
 		}
 	} else {
 		log.Panic("Not supported method", method)
@@ -43,18 +43,14 @@ func (r *RouterBasedOnTree) Route(method string, pattern string, handlerFunc Han
 }
 
 func (r *RouterBasedOnTree) FindHandlerFunc(method string, path string) HandlerFunc {
-	path = strings.Trim(path, "/")
+	path = strings.TrimRight(path, "/")
 	names := strings.Split(path, "/")
 	root, ok := r.forest[method]
 	if ok {
-		if len(names) == 1 && names[0] == "" {
-			return root.getHandlerFunc()
+		if len(names) == 0 {
+			return root.handlerFuncBuilder("", nil)
 		} else {
-			if handlerFunc := root.findHandlerFunc(names); handlerFunc != nil {
-				return handlerFunc
-			} else {
-				return root.getHandlerFunc()
-			}
+			return root.findHandlerFunc(names[0], names[1:])
 		}
 	} else {
 		return nil
@@ -64,7 +60,7 @@ func (r *RouterBasedOnTree) FindHandlerFunc(method string, path string) HandlerF
 func NewRouterBasedOnTree() Router {
 	forest := make(map[string]Node, len(supportMethods))
 	for _, method := range supportMethods {
-		forest[method] = NewExactNode("", nil)
+		forest[method] = NewBaseNode("", nil)
 	}
 	return &RouterBasedOnTree{
 		forest: forest,
@@ -74,53 +70,89 @@ func NewRouterBasedOnTree() Router {
 type BaseNode struct {
 	name        string
 	handlerFunc HandlerFunc
+	children    map[string]Node
 }
 
-type ExactNode struct {
-	BaseNode
-	children map[string]Node
-}
-
-func (n *ExactNode) setHandlerFunc(handlerFunc HandlerFunc) {
-	n.handlerFunc = handlerFunc
-}
-
-func (n *ExactNode) getHandlerFunc() HandlerFunc {
-	return n.handlerFunc
-}
-
-func NewExactNode(name string, handlerFunc HandlerFunc) Node {
-	return &ExactNode{
-		BaseNode: BaseNode{
-			name:        name,
-			handlerFunc: handlerFunc,
-		},
-		children: make(map[string]Node),
+func NewBaseNode(name string, handlerFunc HandlerFunc) Node {
+	return &BaseNode{
+		name:        name,
+		handlerFunc: handlerFunc,
+		children:    make(map[string]Node),
 	}
 }
 
-func (n *ExactNode) addChild(names []string, handlerFunc HandlerFunc) {
+type ParamNode struct {
+	BaseNode
+}
+
+func NewParamNode(name string, handlerFunc HandlerFunc) Node {
+	return &ParamNode{
+		BaseNode: BaseNode{
+			name:        name,
+			handlerFunc: handlerFunc,
+			children:    make(map[string]Node),
+		},
+	}
+}
+
+func (n *ParamNode) handlerFuncBuilder(name string, handlerFunc HandlerFunc) HandlerFunc {
+	return func(c *Context) {
+		c.ParamMap[n.name] = name
+		if handlerFunc != nil {
+			handlerFunc(c)
+		} else {
+			n.handlerFunc(c)
+		}
+	}
+}
+
+func (n *BaseNode) setHandlerFunc(handlerFunc HandlerFunc) {
+	n.handlerFunc = handlerFunc
+}
+
+func (n *BaseNode) handlerFuncBuilder(string, HandlerFunc) HandlerFunc {
+	return n.handlerFunc
+}
+
+func (n *BaseNode) addNode(name string, handlerFunc HandlerFunc) Node {
+	key := name
+	if strings.HasPrefix(key, ":") {
+		key = ":"
+	}
+	var node Node
+	if key == ":" {
+		node = NewParamNode(name[1:], handlerFunc)
+	} else {
+		node = NewBaseNode(name, handlerFunc)
+	}
+	n.children[key] = node
+	return node
+
+}
+
+func (n *BaseNode) addChild(names []string, handlerFunc HandlerFunc) {
 	name := names[0]
 	node := n.findChild(name)
 	if node == nil {
+		var f HandlerFunc
 		if len(names) <= 1 {
-			node = NewExactNode(name, handlerFunc)
-		} else {
-			node = NewExactNode(name, nil)
+			f = handlerFunc
 		}
-		n.children[name] = node
+		node = n.addNode(name, f)
 	}
 	if len(names) > 1 {
 		node.addChild(names[1:], handlerFunc)
 	}
 }
 
-func (n *ExactNode) isMatched(name string) bool {
+func (n *BaseNode) isMatched(name string) bool {
 	return n.name == "*" || n.name == name
 }
 
-func (n *ExactNode) findChild(name string) Node {
+func (n *BaseNode) findChild(name string) Node {
 	if child := n.children[name]; child != nil {
+		return child
+	} else if child := n.children[":"]; child != nil {
 		return child
 	} else if child := n.children["*"]; child != nil {
 		return child
@@ -128,14 +160,19 @@ func (n *ExactNode) findChild(name string) Node {
 	return nil
 }
 
-func (n *ExactNode) findHandlerFunc(names []string) HandlerFunc {
-	if matched := n.findChild(names[0]); matched != nil {
-		if len(names) > 1 {
-			if handlerFunc := matched.findHandlerFunc(names[1:]); handlerFunc != nil {
+func (n *BaseNode) findHandlerFunc(path string, paths []string) HandlerFunc {
+	name := paths[0]
+	if matched := n.findChild(name); matched != nil {
+		if len(paths) > 1 {
+			if handlerFunc := matched.findHandlerFunc(name, paths[1:]); handlerFunc != nil {
 				return handlerFunc
 			}
 		}
-		return matched.getHandlerFunc()
+		return matched.handlerFuncBuilder(name, nil)
 	}
-	return n.getHandlerFunc()
+	return n.handlerFuncBuilder(path, nil)
+}
+
+func (n *ParamNode) findHandlerFunc(path string, paths []string) HandlerFunc {
+	return n.handlerFuncBuilder(path, n.BaseNode.findHandlerFunc(path, paths))
 }
