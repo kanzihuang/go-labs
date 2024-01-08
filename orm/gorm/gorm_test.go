e6d4462ed874ab709a8d12c7b2dc5ac4f71ea203
wanli <kanzihuang@qq.com>
2024-01-09 07:44:03 +0800

    test gorm

--- orm/gorm/gorm_test.go
+++ orm/gorm/gorm_test.go
@@ -8,10 +8,38 @@ import (
 	"testing"
 )
 
+type Payload interface {
+	TableName() string
+}
+
+type PayloadProduct struct {
+	Color string
+}
+
+func newPayloadProduct(color string) *PayloadProduct {
+	return &PayloadProduct{
+		Color: color,
+	}
+}
+
+func (p *PayloadProduct) TableName() string {
+	return "test"
+}
+
 type Product struct {
+	//unsupported data type
+	//Payload
+	*PayloadProduct
+
 	Id    int    `gorm:"column(id),primary""`
 	Name  string `gorm:"column(name)""`
 	Price int    `gorm:"column(price)"`
+}
+
+func newProduct(payload *PayloadProduct) *Product {
+	return &Product{
+		PayloadProduct: payload,
+	}
 }
 
 func (p Product) TableName() string {
@@ -22,7 +50,7 @@ func TestCRUD(t *testing.T) {
 	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
 	require.NoError(t, err, "sqlite open failed: %v", err)
 	db = db.Debug()
-	err = db.AutoMigrate(new(Product))
+	err = db.AutoMigrate(newProduct(newPayloadProduct("red")))
 	require.NoError(t, err, "gorm migrate failed: %v", err)
 	testCases := []struct {
 		name    string
@@ -31,16 +59,24 @@ func TestCRUD(t *testing.T) {
 		product Product
 	}{
 		{
-			name:    "create product",
-			op:      "create",
-			want:    Product{Id: 1, Name: "Phone", Price: 5000},
-			product: Product{Id: 1, Name: "Phone", Price: 5000},
+			name: "create product",
+			op:   "create",
+			want: Product{
+				PayloadProduct: newPayloadProduct("red"),
+				Id:             1, Name: "Phone", Price: 5000},
+			product: Product{
+				PayloadProduct: newPayloadProduct("red"),
+				Id:             1, Name: "Phone", Price: 5000},
 		},
 		{
-			name:    "update product",
-			op:      "update",
-			want:    Product{Id: 1, Name: "Phone", Price: 3000},
-			product: Product{Id: 1, Name: "Phone", Price: 3000},
+			name: "update product",
+			op:   "update",
+			want: Product{
+				PayloadProduct: newPayloadProduct("yellow"),
+				Id:             1, Name: "Phone", Price: 3000},
+			product: Product{
+				PayloadProduct: newPayloadProduct("yellow"),
+				Id:             1, Name: "Phone", Price: 3000},
 		},
 	}
 	for _, tc := range testCases {
